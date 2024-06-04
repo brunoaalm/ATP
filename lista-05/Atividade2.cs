e5b63872ca6c0cb4227d8e2b8366d94e7050834c
Bruno <Bruno@DESKTOP-C77SCRI>
2024-06-04 12:25:17 -0300

    Atividade 2

--- lista-05/Atividade2.cs
+++ lista-05/Atividade2.cs
@@ -2,38 +2,62 @@
 namespace lista_05;
 public class Atividade2
 {
+    public static void Questao()
+    {
+        int[] vetor = new int[10];
+        Preencher(vetor);
+        Imprimir(vetor);
 
-	public void Preencher()
+    }
+
+    public static void Preencher(int[] vetor)
 	{
-		Random n = new Random(11);
+		Random sorteio = new Random();
 		for (int i = 0; i < 10; i++)
 		{
-			vt[n] = n.NextInt64();
-		}
-	}
+			vetor[i] = sorteio.Next(0,10);
+        }
+    }
 
-	public void Imprimir(int vetor[10])
+    public static void Imprimir(int[] vetor)
+    {
+
+        for (int i = 0; i < 10; i++)
+        {
+            int aluno = i + 1;
+            Console.WriteLine("Aluno[{0}]= {1}",aluno, vetor[i]);
+        }
+
+        double media = 0;
+        double soma = 0;
+
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            soma += vetor[i];
+        }
+        media = soma / 10;
+
+        int contador = 0;
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            if (vetor[i] >= 6)
+            {
+                contador++;
+            }
+        }
+        Console.WriteLine("A média dos alunos foram {0} e {1} alunos ficaram acima da média", media, contador);
+    }
+
+    public double CalculaMedia(int[] vetor)
 	{
-		for (int i = 0; i < 10; i++)
-			Console.WriteLine("Vet[" + i"]=" + vetor[i]);
-	}
+        double media = 0;
+        double soma = 0;
 
-	public double CalculaMedia(int vetor[10])
-	{
-		double media = 0;
-		double soma = 0;
-
-		for (int i = 0; i < vetor.Length; i++)
-		{
-			soma += vetor[i];
-		}
-		media = soma / 10;
-		return media;
-	}
-	public static void Questao()
-	{
-		int[] vetor = new int[10];
-		Preencher(vetor);
-		Imprimir(vetor);
-
-	}
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            soma += vetor[i];
+        }
+        media = soma / 10;
+        return media;
+    }
+}
