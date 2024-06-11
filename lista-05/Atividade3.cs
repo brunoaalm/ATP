8f6731b415ad0e687d00f23d6202b9d54d7b8089
Bruno <Bruno@DESKTOP-C77SCRI>
2024-06-10 21:48:52 -0300

    Lab

--- lista-05/Atividade3.cs
+++ lista-05/Atividade3.cs
@@ -2,37 +2,43 @@
 namespace lista_05;
 public class Atividade3
 {
-	static int Retorno(int[] vetor)
+	static int[] Retorno(int[] vetor)
 	{
 		int[] novo = new int[10];
 		VetorPreenchido(vetor);
+		int j = 0;
 		for (int i = 0; i < 10; i++)
 		{
-            if (vetor[i] < 0)
+			if (vetor[i] < 0)
 			{
-				vetor[i] += novo[i];
-				//Console.WriteLine("Vetor novo: "+novo[i]);
-			}
+				novo[j++] = vetor[i];
+                //Console.WriteLine(vetor[i]);
+                //Console.WriteLine("Vetor novo: "+novo[i]);
+            }
 		}
-		
-        return novo[i];
+		return novo;
 	}
 	static void VetorPreenchido(int[] vetor)
 	{
 		Random sorteio = new Random();
 		for (int i = 0; i < 10; i++)
 		{
-			vetor[i] = sorteio.Next(-10,10);
-			Console.WriteLine("Vetor [{0}]: {1}", i,vetor[i]);
+			vetor[i] = sorteio.Next(-10, 10);
 		}
-
 	}
-	public static void Questao()
+	static void Imprimir(int[] vetor)
 	{
-		Console.WriteLine("Faça um procedimento que preencha um vetor X de 10 elementos. A seguir faça uma função\r\nque receba um vetor preenchido, teste e copie todos os valores negativos deste vetor para um\r\nnovo vetor (sem deixar elementos vazios entre os valores copiados), retornando este vetor\r\ncomo resultado. Faça um procedimento que recebe e exibe o conteúdo de um vetor. Faça um\r\nprograma que faça as devidas declarações e acione os módulos para exemplificar o seu uso.");
-		Console.WriteLine();
-
-		int[] vetor = new int[10];
-		Retorno(vetor);
+		for (int i = 0; i < 10; i++)
+		{
+			Console.WriteLine("Vetor: [{0}]",Retorno(vetor));
+		}
 	}
+    public static void Questao()
+    {
+        Console.WriteLine("Faça um procedimento que preencha um vetor X de 10 elementos. A seguir faça uma função\r\nque receba um vetor preenchido, teste e copie todos os valores negativos deste vetor para um\r\nnovo vetor (sem deixar elementos vazios entre os valores copiados), retornando este vetor\r\ncomo resultado. Faça um procedimento que recebe e exibe o conteúdo de um vetor. Faça um\r\nprograma que faça as devidas declarações e acione os módulos para exemplificar o seu uso.");
+        Console.WriteLine();
+
+        int[] vetor = new int[10];
+        Imprimir(vetor);
+    }
 }
